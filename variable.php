d019f9df148b0647330bcd2165851b3c936ee1ae
instocky <instocky@gmail.com>
2016-07-25 08:48:55 +0300

    commit

--- variable.php
+++ variable.php
@@ -15,5 +15,22 @@ echo isset($intVar) . '<br>';
 echo isset($Var) . '<br>';
 
 echo '<h2>Проверка function unset()</h2>';
-unset($intVar);
+// unset($intVar);
 echo isset($intVar) . '<br>';
+
+
+//  Проверка типа переменной если не верно - ничего не выводится, 1-верно
+echo is_integer($intVar) . "<br>";
+echo is_bool($intVar) . " тип " . gettype($intVar);
+
+// Преобразование типов
+echo "<h2>Преобразование типов</h2>";
+
+// Константа
+define('MY_CONST', 100);
+echo MY_CONST . '<br>';
+
+// Условный оператор сравнеия в одну строку
+echo (isset($intVar)) ? 'есть' : 'нет';
+
+echo (2 > 1? "Passed the test " . $intVar : 'Failed the test<br>');
