d699470a898ef931968a1a00a429f8e5793a2605
instocky <instocky@gmail.com>
2016-08-02 23:20:25 +0300

    2homework

---
+++ 08/strlen.php
@@ -0,0 +1,24 @@
+<?php
+// i:\[skladchik] (profit) PHP-1\1 урок 2016-04-04.mp4
+// i:\[skladchik] (profit) PHP-1\php1-08.pdf
+// функции работы со строками
+// strlen lenth string
+
+// echo $var = "hello World!\n";
+// echo "<br>";
+// echo $count = strlen($var);
+// echo "<br>";
+// echo $temp = 'temp@$33.ru';
+// echo "<br>";
+// $dc = 2 . 2;
+var_dump(18 % 4);
+//
+// echo "<br>";
+// echo rand();
+
+$a = 3;
+echo $b = &$a;
+var_dump($b);
+
+echo "<br>";
+var_dump('000002' == '2');
